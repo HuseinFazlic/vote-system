673ca0e528461886d110b61f002ecb7d154a18d9
Husein Fazlić <hfazlic_1@hotmail.com>
2020-04-07 20:31:38 +0200

    prva verzija

---
+++ WebApplication4/WebApplication4/Models/Voter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class Voter
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int JMBG { get; set; }
+
+    }
+}
