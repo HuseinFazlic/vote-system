a952f9c59f43475ae733c2a27b9cfc1f0789fa7d
nejratrle <62563205+nejratrle@users.noreply.github.com>
2020-06-05 12:47:44 +0200

    Add files via upload

---
+++ VoteSystem v2/VoteSystem/VoteSystem/DAO/CandidateListDAO.cs
@@ -0,0 +1,18 @@
+@* The following code was written by Nejra Trle *@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VoteSystem.Models;
+
+namespace VoteSystem.DAO
+{
+    public class CandidateListDAO
+    {
+        sql s = new sql();
+        public List<CandidateList> GetAll()
+        {
+            return s.CandidateList.ToList();
+        }
+    }
+}
